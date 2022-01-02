2dfccdade6c6c3a6dabb8c4dc5ca489ef4b70286
BayuDC <dev.bayudc@gmail.com>
2022-01-02 13:58:20 +0700

    Add landing and home views

--- routes/main.js
+++ routes/main.js
@@ -2,5 +2,6 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => res.render('index'));
+router.get('/home', (req, res) => res.render('home'));
 
 module.exports = router;
